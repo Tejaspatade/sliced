8bcf9b03ec0c347a9d97096067b5387c4e7a0f59
Tejas Patade <tej.patade69@gmail.com>
2023-07-04 18:00:31 +0530

    Loading Animation, Fetch API Data while rendering

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigation } from "react-router-dom";
 
 import CartOverview from "../features/cart/CartOverview";
+import LoadSpinner from "./LoadSpinner";
 
 const AppLayout = () => {
+	// Router-DOM Hooks
+	const navigation = useNavigation();
+
+	// Derived State
+	const isLoading = navigation.state === "loading";
+
 	return (
-		<div>
+		<div className="layout">
+			{isLoading && <LoadSpinner />}
+
 			<Link to="/">Sliced Co.</Link>
 
 			<main>
