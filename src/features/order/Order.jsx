026d8215de67e75fbadd9a36775c4a460950321f
Tejas Patade <tej.patade69@gmail.com>
2023-07-05 13:41:52 +0530

    Order Page Styles

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,67 +1,83 @@
 import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
-	calcMinutesLeft,
-	formatCurrency,
-	formatDate,
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
 // Test ID: IIDSAT
 function Order() {
-	// Router-DOM Hooks
-	const order = useLoaderData();
-	const {
-		// id,
-		status,
-		priority,
-		priorityPrice,
-		orderPrice,
-		estimatedDelivery,
-		// cart,
-	} = order;
-	const deliveryIn = calcMinutesLeft(estimatedDelivery);
+  // Router-DOM Hooks
+  const order = useLoaderData();
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order;
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
-	return (
-		<div>
-			<div>
-				<h2>Status</h2>
+  return (
+    <div className="space-y-8 px-4 py-10  ">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-2xl font-bold">Order No. {id} Status</h2>
 
-				<div>
-					{priority && <span>Priority</span>}
-					<span>{status} order</span>
-				</div>
-			</div>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-600 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-100">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-100">
+            {status} order
+          </span>
+        </div>
+      </div>
 
-			<div>
-				<p>
-					{deliveryIn >= 0
-						? `Only ${calcMinutesLeft(
-								estimatedDelivery
-						  )} minutes left 😃`
-						: "Order should have arrived"}
-				</p>
-				<p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-			</div>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 p-5">
+        <p className="font-medium">
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            : "Order should have arrived"}
+        </p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
+      </div>
 
-			<div>
-				<p>Price pizza: {formatCurrency(orderPrice)}</p>
-				{priority && (
-					<p>Price priority: {formatCurrency(priorityPrice)}</p>
-				)}
-				<p>
-					To pay on delivery:{" "}
-					{formatCurrency(orderPrice + priorityPrice)}
-				</p>
-			</div>
-		</div>
-	);
+      <ul className="divide-y-2 divide-stone-200 border-b-2 border-t-2 border-stone-200">
+        {cart.map((pizza) => (
+          <OrderItem key={pizza.pizzaId} item={pizza} />
+        ))}
+      </ul>
+
+      <div className="space-y-5 bg-stone-200 p-5">
+        <p className="text-sm font-medium text-stone-500">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium text-stone-500">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="text-m font-bold text-stone-800">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 // Loader For Routing
 export const loader = async ({ params }) => {
-	const order = await getOrder(params.orderId);
-	return order;
+  const order = await getOrder(params.orderId);
+  return order;
 };
 
 export default Order;
