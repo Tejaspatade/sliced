9b30831e8541b6878a8f6809db385b40e0905441
Tejas Patade <tej.patade69@gmail.com>
2023-07-06 09:57:55 +0530

    Styled Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import UserName from "../features/user/UserName";
 const Header = () => {
   return (
     <header className="flex items-center justify-between border-b-2 border-stone-700 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
+      <Link to="/" className="text-xl font-extrabold tracking-widest">
         Sliced Co.
       </Link>
       <SearchOrders />
