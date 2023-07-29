24725c4c8903d85e9df33c1983aaf24e11b0d9a7
Tejas Patade <tej.patade69@gmail.com>
2023-07-29 12:56:49 +0530

    Logo Styles & Minor Debugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between border-b-2 border-stone-700 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="text-xl font-extrabold tracking-widest">
-        Sliced Co.
-        <img src="/Sliced.png" alt="Sliced Logo" />
+        <img src="/Sliced.png" alt="Sliced Logo" className="w-36" />
       </Link>
       <SearchOrders />
       <UserName />
--- src/features/order/SearchOrders.jsx
+++ src/features/order/SearchOrders.jsx
@@ -18,7 +18,7 @@ const SearchOrders = () => {
     <form onSubmit={handleSubmit}>
       <input
         className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:font-semibold placeholder:text-stone-600 focus:outline-none focus:ring focus:ring-yellow-400 sm:w-64 sm:focus:w-72"
-        placeholder="Search Orders Here..."
+        placeholder="Search Your Order No Here..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       ></input>
